836fb5063c7d20f3db5b7b1838fb1a7317b44222
Dylan Salinas <Dylan.salinas108@gmail.com>
2025-07-04 22:15:45 -0300

    partiendo desde proyecto integral

---
+++ Front end/html talentotech/javascrpt/scripts.js
@@ -0,0 +1,16 @@
+//esto es un mensaje en la consola
+console.log("holamundo")
+
+//Declaracion de variables
+let nombre = "Gabriela"
+let edad = 26
+
+nombre = 45
+
+console.log(nombre,edad)
+console.log(edad)
+
+// string nombre = "camila"
+
+
+//error de tipo, no puede guardar un entero en una variable tipo string
